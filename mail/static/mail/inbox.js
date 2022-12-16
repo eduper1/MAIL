e4386e7e30d761dc80a22fcd674946b3606495a1
collotito <titocollins1@gmail.com>
2022-12-16 09:27:11 +0300

    mail can be sent

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
+  document.querySelector('#compose-form').addEventListener('submit', send_mail);
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -30,4 +31,27 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+}
+
+function send_mail(event) {
+  event.preventDefault();
+  console.log("hello");
+  const receiver = document.querySelector('#compose-recipients').value;
+  const title = document.querySelector('#compose-subject').value;
+  const body = document.querySelector('#compose-body').value;
+  console.log(receiver, title, body);
+
+  // Send data to the API url
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+      recipients: receiver,
+      subject: title,
+      body: body
+    })
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+  });
 }

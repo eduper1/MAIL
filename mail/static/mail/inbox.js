8ff0399beee10aeb570759c18fabfe5c07b52847
collotito <titocollins1@gmail.com>
2023-01-06 17:21:21 +0300

    email are functionally displayed but not orgainzed

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+  
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
   document.querySelector('#compose-form').addEventListener('submit', send_mail);
-
+  
   // By default, load the inbox
   load_mailbox('inbox');
 });
@@ -15,6 +15,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -23,12 +24,32 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
+// function to display an email's body
+function displayMail(email_id){
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    console.log(email);
+    // Hide all other views.
+    document.querySelector('#email-view').style.display = 'block';
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    
+    // display each email
+    // const display = document.createElement('p');
+    document.querySelector('#email-view').innerHTML = `id: ${email.id}, body: ${email.body}`;
+    // document.querySelector('#email-view').append(display);
+  });
+}
+
 // function to display all emails in a mailbox
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -41,42 +62,21 @@ function load_mailbox(mailbox) {
     emails.forEach(email => {
       // console.log(email.sender, email.subject, email.timestamp)
       // Display message on the screen
-      // document.querySelector('#emails-view').innerHTML = `Sender: ${email.sender}; subject: ${email.subject}; Timestamp: ${email.timestamp}`;
       const elementDiv = document.createElement('div');
-      elementDiv.setAttribute('id', 'msg');
+      // elementDiv.setAttribute('id', 'msg');
       elementDiv.style.backgroundColor = "#ffffff";
       
-      const elementA = document.createElement('a');
-      elementA.innerHTML = `Sender: ${email.sender}; subject: ${email.subject}; Timestamp: ${email.timestamp}`;
-      elementA.setAttribute('href', '#');
-      elementA.addEventListener('click', displayMail(email.id));
-      // element.addEventListener('click', function() {
-        // console.log('This element has been clicked!')
-        // });
-      elementDiv.appendChild(elementA);
+      elementDiv.innerHTML = `Sender: ${email.sender}; subject: ${email.subject}; Timestamp: ${email.timestamp}`;
+      elementDiv.addEventListener('click', () =>{
+        console.log('This element is clicked');
+        displayMail(email.id);
+      });
       document.querySelector('#emails-view').append(elementDiv);
     });
-
-
-
   });
   return false;
 }
 
-// function to display an email's body
-function displayMail(email_id){
-  fetch(`/emails/${email_id}`)
-  .then(response => response.json())
-  .then(email => {
-    console.log(email);
-    // display each email
-    const display = document.createElement('div');
-    display.innerHTML = `id: ${email.id}, body: ${email.body}`;
-    document.querySelector('#emails-view').append(display);
-  });
-}
-
-// console.log(displayMail());
 
 // function to send emails
 function send_mail(event) {
